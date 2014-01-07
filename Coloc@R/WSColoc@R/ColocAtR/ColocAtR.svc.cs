51d93242e97c5afe4a1a46d93466f09f5677af20
Maxence Adnot <adnot.maxence@gmail.com>
2014-01-07 20:14:14 +0100

    Ajouts méthodes Coloc@R

--- Coloc@R/WSColoc@R/ColocAtR/ColocAtR.svc.cs
+++ Coloc@R/WSColoc@R/ColocAtR/ColocAtR.svc.cs
@@ -13,14 +13,28 @@ namespace WSColocAtR
         DataClassesDataContext data = new DataClassesDataContext();
         RemoteEndpointMessageProperty endpoint = (RemoteEndpointMessageProperty)OperationContext.Current.IncomingMessageProperties[RemoteEndpointMessageProperty.Name];
 
+        #region Auth
+
+        public WSAuthMessage CreateAccount(string login, string email, string password, string firstName, string lastName)
+        {
+            WSAuthMessage Response = new WSAuthMessage();
+
+            // TODO : Creation compte
+
+            Response.StatusCode = StatusCode.OK;
+            Response.Data = "Code de retour";
+
+            return Response;
+        }
+
         public WSAuthMessage SignIn(string email, string password)
         {
             WSAuthMessage Response = new WSAuthMessage();
 
             // Vérifie que le couple email/password correspond en base de données
             var usersList = (from users in data.Users
-                        where users.emailUser == email && users.passwordUser == Utils.ToSha256(password)
-                        select users);
+                             where users.emailUser == email && users.passwordUser == Utils.ToSha256(password)
+                             select users);
 
             if (usersList.Count() != 1)
             {
@@ -93,7 +107,7 @@ namespace WSColocAtR
 
             Response.StatusCode = StatusCode.OK;
             Response.Data = "Deconnecté";
- 
+
             return Response;
         }
 
@@ -133,48 +147,6 @@ namespace WSColocAtR
             return Response;
 
         }
-
-        public WSAuthMessage Whoami(string token)
-        {
-            WSAuthMessage Response = new WSAuthMessage();
-
-            
-            if (RefreshLastActivity(token).StatusCode == StatusCode.OK)
-            {
-                var session = GetSession(token);
-                
-                var userLogin = (from users in data.Users
-                            where users.idUser == session.idUser
-                            select users.loginUser).FirstOrDefault();
-                
-                Response.StatusCode = StatusCode.OK;
-                Response.Data = userLogin == null ? "inconnu" : userLogin;
-            }
-
-            return Response;
-        }
-
-        public WSAuthMessage Kevbac(string token)
-        {
-            WSAuthMessage Response = new WSAuthMessage();
-
-
-            if (RefreshLastActivity(token).StatusCode == StatusCode.OK)
-            {
-                var session = GetSession(token);
-
-                var userAge = (from users in data.Users
-                                 where users.idUser == session.idUser
-                                 select users.age).FirstOrDefault();
-
-                Response.StatusCode = StatusCode.OK;
-                Response.Data = (userAge == null ? -1 : userAge).ToString();
-            }
-
-            return Response;
-        }
-
-
         bool CheckIfOnline(string token)
         {
             bool online = false;
@@ -204,5 +176,80 @@ namespace WSColocAtR
             }
             return session.First();
         }
+
+        public WSAuthMessage Whoami(string token)
+        {
+            WSAuthMessage Response = new WSAuthMessage();
+
+
+            if (RefreshLastActivity(token).StatusCode == StatusCode.OK)
+            {
+                var session = GetSession(token);
+
+                var userLogin = (from users in data.Users
+                                 where users.idUser == session.idUser
+                                 select users.loginUser).FirstOrDefault();
+
+                Response.StatusCode = StatusCode.OK;
+                Response.Data = userLogin == null ? "inconnu" : userLogin;
+            }
+
+            return Response;
+        }
+
+        #endregion
+
+        #region ProfilManager
+
+        public WSAuthMessage ProfilWizard(string token, bool type, int age, int price, string city, string desc, int m2 = 0)
+        {
+            WSAuthMessage Response = new WSAuthMessage();
+
+            if (RefreshLastActivity(token).StatusCode == StatusCode.OK)
+            {
+                var session = GetSession(token);
+
+                // TODO : Remplissage du profil
+
+                Response.StatusCode = StatusCode.OK;
+                Response.Data = "Code de retour";
+            }
+            return Response;
+        }
+
+        public WSAuthMessage ChangePassword(string token, string oldPassword, string newPassword)
+        {
+            WSAuthMessage Response = new WSAuthMessage();
+
+            if (RefreshLastActivity(token).StatusCode == StatusCode.OK)
+            {
+                var session = GetSession(token);
+
+                // TODO : Changement du mot de passe
+
+                Response.StatusCode = StatusCode.OK;
+                Response.Data = "Code de retour";
+            }
+            return Response;
+        }
+
+        #endregion
+
+        public WSAuthMessage GetScoringResults(string token)
+        {
+            WSAuthMessage Response = new WSAuthMessage();
+
+            if (RefreshLastActivity(token).StatusCode == StatusCode.OK)
+            {
+                var session = GetSession(token);
+
+                // TODO : Check Scoring
+
+                Response.StatusCode = StatusCode.OK;
+                Response.Data = "Code de retour";
+            }
+            return Response;
+        }
+
     }
 }
--- Coloc@R/WSColoc@R/ColocAtR/IColocAtR.cs
+++ Coloc@R/WSColoc@R/ColocAtR/IColocAtR.cs
@@ -6,6 +6,13 @@ namespace WSColocAtR
     public interface IColocAtR
     {
 
+        #region Auth
+
+        //
+        // Permet de créer un compte avec pour parametre un nom d'utilisateur, un mot de passe, un prénom et un nom
+        [OperationContract]
+        WSAuthMessage CreateAccount(string login, string email, string password, string firstName, string lastName);
+
         //
         // Permet à l'utilisateur de creer une session et de se connecter grâce à son email / mot de passe.
         [OperationContract]
@@ -21,11 +28,33 @@ namespace WSColocAtR
         [OperationContract]
         WSAuthMessage RefreshLastActivity(string token);
 
+        //
         // Retourne le nom de l'utilisateur
         [OperationContract]
         WSAuthMessage Whoami(string token);
 
+        #endregion
+
+        #region ProfilManager
+
+
+        //
+        // Permet de mettre à jour le profil complet
         [OperationContract]
-        WSAuthMessage Kevbac(string token);
+        WSAuthMessage ProfilWizard(string token, bool type, int age, int price, string city, string desc, int m2 = 0);
+
+        //
+        // Permet de changer le mot de passe de l'utilisateur connecté
+        [OperationContract]
+        WSAuthMessage ChangePassword(string token, string oldPassword, string newPassword);
+
+
+        //
+        // Retourne les annonces correspondantes aux critères de l'utilisateur
+        [OperationContract]
+        WSAuthMessage GetScoringResults(string token);
+
+        #endregion
+
     }
 }
